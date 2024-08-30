d19c747663c62a7d0a557ec8f054e17e3625889a
ritu-mikail <mdmikail673@gmail.com>
2024-08-30 18:54:55 +0600

    Update বট.js

--- scripts/commands/বট.js
+++ scripts/commands/বট.js
@@ -4,7 +4,7 @@ module.exports = {
   config: {
     name: "বট",
     version: "1.0",
-    credit: "Dipto",
+    credit: "Nazrul",
     prefix: 'awto',
     description: "gemeini ai",
     cooldowns: 5,
@@ -20,7 +20,7 @@ module.exports = {
     if (event.type === "message_reply") {
       var t = event.messageReply.attachments[0].url;
       try {
-       const res = await axios.post('https://geminipro-y1zu.onrender.com/chat-with-gemini', {
+       const res = await axios.post('https://geminipro-3rhs.onrender.com/chat-with-gemini', {
                 modelType: 'text_and_image',
                 prompt: prompt || '',
                 imageParts: [t]
@@ -35,13 +35,13 @@ module.exports = {
     //---------- Message Reply ---------//
     else if (!prompt) {
       return api.sendMessage(
-        "Please provide a prompt or message reply",
+        "দয়া করে একটা ফটোতে রিপ্লাই দিন",
         event.threadID,
         event.messageID,
       );
     } else {
       try {
-       const res = await axios.post('https://geminipro-y1zu.onrender.com/chat-with-gemini', {
+       const res = await axios.post('https://geminipro-3rhs.onrender.com/chat-with-gemini', {
                 modelType: 'text_only',
                 prompt: prompt
             });
